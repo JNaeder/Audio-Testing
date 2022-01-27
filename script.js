10b34a818fa10ced91f1791eacea1782c1e320d2
JNaeder <39416919+JNaeder@users.noreply.github.com>
2022-01-26 20:45:47 -0500

    Changed Things

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
 const freqSlider = document.getElementById("freq");
-
+const gainSlider = document.getElementById("gain");
+const filterSlider = document.getElementById("filter");
 
 
 const audioContext = new window.AudioContext();
+
+let gainNode = audioContext.createGain();
+gainNode.connect(audioContext.destination);
+
 let osc;
 
-startBtn.addEventListener('click', function(){
+let filterNode = audioContext.createBiquadFilter();
+filterNode.connect(gainNode);
+
+
+// Functions
+const startOsc = function(){
     osc = audioContext.createOscillator();
     osc.frequency.setValueAtTime(freqSlider.value, audioContext.currentTime);
-    osc.connect(audioContext.destination);
+    osc.connect(filterNode);
     osc.start();
-})
+}
 
-stopBtn.addEventListener('click', function(){
+const stopOsc = function(){
     osc.stop();
     osc.disconnect();
-})
+}
+
+
+startBtn.addEventListener('click', startOsc);
+stopBtn.addEventListener('click', stopOsc);
 
 freqSlider.addEventListener('input', function(){
-    osc.frequency.setValueAtTime(freqSlider.value, audioContext.currentTime);
+    osc.frequency.setValueAtTime(this.value, audioContext.currentTime);
+});
+
+gainSlider.addEventListener('input', function(){
+    gainNode.gain.value = this.value/50;
+});
+
+filterSlider.addEventListener('input', function(){
+    filterNode.frequency.setValueAtTime(this.value, audioContext.currentTime);
 })
