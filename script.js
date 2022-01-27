54ef693777c42be75688413ab8101ce2041dd28b
JNaeder <j.naeder324@gmail.com>
2022-01-27 09:18:08 -0500

    Added waveform

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const oscStartBtn = document.getElementById("osc_start");
 const oscStopBtn = document.getElementById("osc_stop");
 const audioStartBtn = document.getElementById("audio_start");
 const audioStopBtn = document.getElementById("audio_stop");
+const waveSlider = document.getElementById("wave");
 const freqSlider = document.getElementById("freq");
 const gainSlider = document.getElementById("gain");
 const filterSlider = document.getElementById("filter");
 const resonanceSlider = document.getElementById("resonance");
-
 
 const audioContext = new window.AudioContext();
 
@@ -22,60 +22,79 @@ console.log(filterNode);
 
 let randomGen;
 
-
 // Functions
-const startOsc = function(){
-    osc = audioContext.createOscillator();
-    osc.frequency.setValueAtTime(freqSlider.value, audioContext.currentTime);
-    osc.connect(filterNode);
-    console.log(osc);
-    randomGen = setInterval(randomNotes, 100);
-    osc.type = "sawtooth";
-    osc.start();
-}
+const startOsc = function () {
+  osc = audioContext.createOscillator();
+  osc.frequency.setValueAtTime(freqSlider.value, audioContext.currentTime);
+  osc.connect(filterNode);
+  console.log(osc);
+  randomGen = setInterval(randomNotes, 100);
+  osc.type = "sawtooth";
+  osc.start();
+};
 
-const stopOsc = function(){
-    osc.stop();
-    osc.disconnect();
-    clearInterval(randomGen);
-    // console.log(osc);
-}
+const stopOsc = function () {
+  osc.stop();
+  osc.disconnect();
+  clearInterval(randomGen);
+  // console.log(osc);
+};
 
-const startAudio = function(){
-    console.log("Start");
-}
+const startAudio = function () {
+  console.log("Start");
+};
 
-const stopAudio = function(){
-    console.log("Stop");
-}
+const stopAudio = function () {
+  console.log("Stop");
+};
 
-const randomNotes = function(){
-    const randomNum = Math.trunc(Math.random() * 1000);
-    osc.frequency.setValueAtTime(randomNum, audioContext.currentTime);
-    freqSlider.value = randomNum;
-}
+const randomNotes = function () {
+  const randomNum = Math.trunc(Math.random() * 1000);
+  osc.frequency.setValueAtTime(randomNum, audioContext.currentTime);
+  freqSlider.value = randomNum;
+};
 
+oscStartBtn.addEventListener("click", startOsc);
+oscStopBtn.addEventListener("click", stopOsc);
+audioStartBtn.addEventListener("click", startAudio);
+audioStopBtn.addEventListener("click", stopAudio);
 
-
-oscStartBtn.addEventListener('click', startOsc);
-oscStopBtn.addEventListener('click', stopOsc);
-audioStartBtn.addEventListener('click', startAudio);
-audioStopBtn.addEventListener('click', stopAudio);
-
-freqSlider.addEventListener('input', function(){
-    osc.frequency.setValueAtTime(this.value, audioContext.currentTime);
+waveSlider.addEventListener("input", function () {
+  let newWave;
+  switch (Number(this.value)) {
+    case 1:
+      newWave = "sine";
+      break;
+    case 2:
+      newWave = "sawtooth";
+      break;
+    case 3:
+      newWave = "square";
+      break;
+    case 4:
+      newWave = "triangle";
+      break;
+    default:
+      newWave = "sine";
+  }
+  console.log(newWave);
+  osc.type = newWave;
 });
 
-gainSlider.addEventListener('input', function(){
-    gainNode.gain.value = this.value/50;
+freqSlider.addEventListener("input", function () {
+  osc.frequency.setValueAtTime(this.value, audioContext.currentTime);
 });
 
-filterSlider.addEventListener('input', function(){
-    filterNode.frequency.setValueAtTime(this.value, audioContext.currentTime);
-})
+gainSlider.addEventListener("input", function () {
+  gainNode.gain.value = this.value / 50;
+});
 
-resonanceSlider.addEventListener('input', function(){
-    filterNode.Q.setValueAtTime(this.value, audioContext.currentTime);
-    console.log(this.value);
-    console.log(filterNode.Q);
-})
+filterSlider.addEventListener("input", function () {
+  filterNode.frequency.setValueAtTime(this.value, audioContext.currentTime);
+});
+
+resonanceSlider.addEventListener("input", function () {
+  filterNode.Q.setValueAtTime(this.value, audioContext.currentTime);
+  console.log(this.value);
+  console.log(filterNode.Q);
+});
